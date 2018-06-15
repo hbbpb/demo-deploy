3c3b01723c58a43557e1a69face5ac8417523e43
pbli <pbli@thoughtworks.com>
2018-06-15 18:59:32 +0800

    add webui service.

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 COM=$1
 NAMESPACE=$2
-TAG=$3
+IMAGE=$3
 SVC_PORT=$4
 
 function load_template {
@@ -19,6 +19,6 @@ function load_template {
 load_template $COM
 load_template ${COM}-svc
 
-kubectl --server $K8S_MASTER_HOST apply -f tmp/$COM.json
-kubectl --server $K8S_MASTER_HOST apply -f tmp/${COM}-svc.json
+kubectl apply -f tmp/$COM.json
+kubectl apply -f tmp/${COM}-svc.json
 rm -rf tmp/*.json
